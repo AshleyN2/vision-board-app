8dc82aa660d934119c7b31b1c2f327e24fb66987
Ashley <ashleynjeri7@gmail.com>
2023-01-27 23:53:37 +0300

    handle submt function

--- App.js
+++ App.js
@@ -2,3 +2,9 @@ const visionForm = document.getElementById('vision-form');
 const visionInput = document.getElementById('vision-input'); 
 
 // Handle form submission
+visionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const vision = visionInput.value;
+    visionInput.value = '';
+    socket.emit('vision', vision);
+});
